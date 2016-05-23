f8b7ed155b617f42c6cef77082a2c631f256a03a
Avi Solomon <genepool99@gmail.com>
2016-05-22 21:47:52 -0700

    make handler more complete
    Add base class and actual handle function

--- handle_it.py
+++ handle_it.py
@@ -1,17 +1,48 @@
-import web
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
 
-urls = ('/.*', 'hooks')
+import BaseHTTPServer
+import sys
+import time
+import urlparse
+import json
 
-app = web.application(urls, globals())
 
-class hooks:
-    def POST(self):
-        data = web.data()
-        print
-        print 'DATA RECEIVED:'
-        print data
-        print
-        return 'OK'
+HOST_NAME = sys.argv[1]
+PORT_NUMBER = int(sys.argv[2])
+
+
+def handle_hook(payload):
+    """ This is the actual handler for hooks """
+    pass
+
+
+class HookHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    """ Base class to handle hooks forked from FiloSottile/HookHandler.py"""
+    server_version = "simplehandler/1.0"
+    def do_GET(s):
+        s.send_response(200)
+        s.wfile.write('Hello! Talk post to me baby :)')
+
+    def do_POST(s):
+        # Uncomment to check that the IP is within the ranges
+        #if not any(s.client_address[0].startswith(IP) for IP in ('192.168.1', '10.10.10')):
+        #    s.send_error(403)
+
+        length = int(s.headers['Content-Length'])
+        content = s.rfile.read(length)
+        payload = json.loads(content)       # assume content is JSON
+        handle_hook(payload)
+        s.send_response(200)
+
 
 if __name__ == '__main__':
-    app.run()
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), HookHandler)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
